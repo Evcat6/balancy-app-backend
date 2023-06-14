b8dd4fbf977b15df3b3b91237e53af82e1edacec
Pavlo B <distrepid@gmail.com>
2023-06-14 23:04:22 +0300

    feature: Migrate from MySQL database to PostgreSQL

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,7 +2,7 @@ export default () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
   database: {
     host: process.env.DATABASE_HOST,
-    port: 3306,
+    port: process.env.DATABASE_PORT,
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     name: process.env.DATABASE_NAME,
