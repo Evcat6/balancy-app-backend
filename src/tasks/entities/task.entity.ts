3920fcf2843fece37f783c053539860e38381fd0
Nataly <97055353+Nataliya1508@users.noreply.github.com>
2023-06-20 22:03:19 +0300

    add task entity

--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,1 +1,54 @@
-export class Task {}
+// import { User } from 'src/users/entities/user.entity';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+enum Type {
+  Task = 'task',
+  Event = 'event',
+}
+
+enum Prioryty {
+  High = 'high',
+  Medium = 'medium',
+  Low = 'low',
+  Urgent = 'urgent',
+  Normal = 'normal',
+  NonEssential = 'non-essential',
+  Routine = 'routine',
+}
+
+@Entity({ name: 'tasks' })
+export class TaskEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'enum', enum: Type, default: Type.Task })
+  tipe: Type;
+
+  @Column()
+  title: string;
+
+  @Column({ default: '' })
+  description: string;
+
+  @Column('simple-array')
+  category: string[];
+
+  @Column()
+  subcategory: string;
+
+  @Column({ type: 'enum', enum: Prioryty, default: Prioryty.Medium })
+  prioryty: Prioryty;
+
+  @Column({ default: true })
+  pinned: boolean;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createDate: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  completeDate: Date;
+
+  // @ManyToOne(() => User, (user) => user.tasks)
+  // username: User;
+}
+
